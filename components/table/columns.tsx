b0bca329e060f0ea7b0cbd30f3eb2475fd8e7732
Raviyaa27 <ravinduachintha@gmail.com>
2024-10-23 03:02:08 +0530

    enable sms notifications

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -1,88 +1,100 @@
 "use client";
 
-import { ColumnDef } from "@tanstack/react-table";  
-import { Button } from "../ui/button";
-import { MoreHorizontal } from "lucide-react";
-import StatusBadge from "../StatusBadge";
-import { formatDateTime } from "@/lib/utils";
-import { Doctors } from "@/constants";
+import { ColumnDef } from "@tanstack/react-table";
 import Image from "next/image";
-import AppointmentModel from "../AppointmentModel";
+
+import { Doctors } from "@/constants";
+import { formatDateTime } from "@/lib/utils";
 import { Appointment } from "@/types/appwrite.types";
+import StatusBadge from "../StatusBadge";
+import AppointmentModel from "../AppointmentModel";
 
 
 export const columns: ColumnDef<Appointment>[] = [
   {
-    header: "ID",
-    cell: ({ row }) => <p className="text-14-medium">{row.index + 1}</p>,
+    header: "#",
+    cell: ({ row }) => {
+      return <p className="text-14-medium ">{row.index + 1}</p>;
+    },
   },
   {
     accessorKey: "patient",
     header: "Patient",
-    cell: ({ row }) => (
-      <p className="text-14-medium">{row.original.patient.name}</p>
-    ),
+    cell: ({ row }) => {
+      const appointment = row.original;
+      return <p className="text-14-medium ">{appointment.patient.name}</p>;
+    },
   },
   {
     accessorKey: "status",
     header: "Status",
-    cell: ({ row }) => (
+    cell: ({ row }) => {
+      const appointment = row.original;
+      return (
         <div className="min-w-[115px]">
-            <StatusBadge status={row.original.status}/>
+          <StatusBadge status={appointment.status} />
         </div>
-    )  
+      );
+    },
   },
   {
     accessorKey: "schedule",
     header: "Appointment",
-    cell: ({ row }) => (
+    cell: ({ row }) => {
+      const appointment = row.original;
+      return (
         <p className="text-14-regular min-w-[100px]">
-            {formatDateTime(row.original.schedule).dateTime}
+          {formatDateTime(appointment.schedule).dateTime}
         </p>
-    )
+      );
+    },
   },
   {
     accessorKey: "primaryPhysician",
-    header: () => 'Doctor',
+    header: "Doctor",
     cell: ({ row }) => {
-        const doctor = Doctors.find((doc) => doc.name === row.original.primaryPhysician)
+      const appointment = row.original;
 
-        return (
-            <div className="flex items-center gap-3">
-                <Image 
-                    src={doctor?.image}
-                    alt={doctor.name}
-                    width={100}
-                    height={100}
-                    className="size-8"
-                />
-                <p className="whitespace-nowrap">
-                    Dr. {doctor?.name}
-                </p>
-            </div>
-        )
+      const doctor = Doctors.find(
+        (doctor) => doctor.name === appointment.primaryPhysician
+      );
+
+      return (
+        <div className="flex items-center gap-3">
+          <Image
+            src={doctor?.image!}
+            alt="doctor"
+            width={100}
+            height={100}
+            className="size-8"
+          />
+          <p className="whitespace-nowrap">Dr. {doctor?.name}</p>
+        </div>
+      );
     },
   },
   {
     id: "actions",
     header: () => <div className="pl-4">Actions</div>,
-    cell: ({ row: { original: data} }) => {
-        return (
-            <div className="flex gap-1">
-                <AppointmentModel 
-                type="schedule"
-                patientId={data.patient.$id}
-                userId={data.userId}
-                appointment={data}
-                />
-                <AppointmentModel 
-                type="cancel"
-                patientId={data.patient.$id}
-                userId={data.userId}
-                appointment={data}
-                />
-            </div>
-        )
+    cell: ({ row }) => {
+      const appointment = row.original;
+
+      return (
+        <div className="flex gap-1">
+          <AppointmentModel
+            patientId={appointment.patient.$id}
+            userId={appointment.userId}
+            appointment={appointment}
+            type="schedule"
+          />
+          <AppointmentModel
+            patientId={appointment.patient.$id}
+            userId={appointment.userId}
+            appointment={appointment}
+            type="cancel"
+          />
+        </div>
+      );
     },
   },
 ];
--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,8 +1,8 @@
 'use server';
 
 import { ID, Query } from "node-appwrite";
-import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases } from "../appwrite.config";
-import { parseStringify } from "../utils";
+import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases, messaging } from "../appwrite.config";
+import { formatDateTime, parseStringify } from "../utils";
 import { Appointment } from "@/types/appwrite.types";
 import { revalidatePath } from "next/cache";
 
@@ -51,47 +51,74 @@ export const getRecentAppointmentList = async () => {
             cancelledCount: 0,
         }
 
-        const counts = (appointments.documents as Appointment[]).reduce((acc, appointment) => {
-            if (appointment.status === 'scheduled') {
-                acc.scheduledCount += 1;
-            } else if (appointment.status === 'pending') {
-                acc.pendingCount += 1;
-            } else if (appointment.status === 'cancelled') {
-                acc.cancelledCount += 1;
-            }
-            return acc;
-        }, initialCounts);
-
-        const data = {
+        const counts = (appointments.documents as Appointment[]).reduce(
+            (acc, appointment) => {
+              switch (appointment.status) {
+                case "scheduled":
+                  acc.scheduledCount++;
+                  break;
+                case "pending":
+                  acc.pendingCount++;
+                  break;
+                case "cancelled":
+                  acc.cancelledCount++;
+                  break;
+              }
+              return acc;
+            },
+            initialCounts
+          );
+      
+          const data = {
             totalCount: appointments.total,
             ...counts,
-            documents: appointments.documents
-        }
-
-        return parseStringify(data);
+            documents: appointments.documents,
+          };
+      
+          return parseStringify(data);
     } catch (error) {
         console.log(error)
     }
 };
 
-export const updateAppointment = async ({ appointmentId, userId, appointment, type}: UpdateAppointmentParams) => {
+export const updateAppointment = async ({
+    appointmentId,
+    userId,
+    appointment,
+    type,
+  }: UpdateAppointmentParams) => {
     try {
-        const updatedAppointment = await databases.updateDocument(
-            DATABASE_ID!,
-            APPOINTMENT_COLLECTION_ID!,
-            appointmentId,
-            appointment
-        )
-
-        if(!updatedAppointment) {
-            throw new Error('Appointment not found');
-        }
-
-        //SMS notification
-
-        revalidatePath('./admin');
-        return parseStringify(updateAppointment)
+      // Update appointment to scheduled -> https://appwrite.io/docs/references/cloud/server-nodejs/databases#updateDocument
+      const updatedAppointment = await databases.updateDocument(
+        DATABASE_ID!,
+        APPOINTMENT_COLLECTION_ID!,
+        appointmentId,
+        appointment
+      );
+  
+      if (!updatedAppointment) throw Error;
+  
+      const smsMessage = `Greetings from CarePulse. ${type === "schedule" ? `Your appointment is confirmed for ${formatDateTime(appointment.schedule!).dateTime} with Dr. ${appointment.primaryPhysician}` : `We regret to inform that your appointment for ${formatDateTime(appointment.schedule!).dateTime} is cancelled. Reason:  ${appointment.cancellationReason}`}.`;
+      await sendSMSNotification(userId, smsMessage);
+  
+      revalidatePath("/admin");
+      return parseStringify(updatedAppointment);
     } catch (error) {
-        console.log(error)
+      console.error("An error occurred while scheduling an appointment:", error);
     }
-}
+  };
+
+export const sendSMSNotification = async (userId: string, content: string) => {
+    try {
+      // https://appwrite.io/docs/references/1.5.x/server-nodejs/messaging#createSms
+      const message = await messaging.createSms(
+        ID.unique(),
+        content,
+        [],
+        [userId]
+      );
+      return parseStringify(message);
+    } catch (error) {
+      console.error("An error occurred while sending sms:", error);
+    }
+  };
