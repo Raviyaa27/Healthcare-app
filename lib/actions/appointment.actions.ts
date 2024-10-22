c9dab9ebb9c4366759db5c1539b0b9f1efd79660
Raviyaa27 <ravinduachintha@gmail.com>
2024-10-22 16:24:04 +0530

    create success page and admin passcode verification

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -17,4 +17,18 @@ export const createAppointment = async (appointment: CreateAppointmentParams) =>
     } catch (error) {
         console.log(error)
     }
-}
+};
+
+export const getAppointment = async (appointmentId: string) => {
+    try {
+        const appointment = await databases.getDocument(
+            DATABASE_ID!,
+            APPOINTMENT_COLLECTION_ID!,
+            appointmentId,
+        );
+
+        return parseStringify(appointment);
+    } catch (error) {
+        console.log("An error occurred while retrieving the existing patient:", error);
+    }
+};
